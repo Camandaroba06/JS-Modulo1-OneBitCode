279bd1e08278aba03b16fb546552ab9626159531
Camandaroba06 <cristianogregorymc@gmail.com>
2022-09-13 12:49:41 -0300

    finalizado Exercicio3

---
+++ Exercicio3/index.js
@@ -0,0 +1,31 @@
+let medida = parseFloat(window.prompt("Digite uma medida em metros:"));
+let escolha = window.prompt("Escolha a conversão:");
+let valorFinal;
+switch(escolha){
+    case("mm"):
+        valorFinal = medida*1000;
+        break;
+    case("cm"):
+        valorFinal = medida*100;
+        alert(valorFinal);
+        break;
+    case("dm"):
+        valorFinal = medida*10;
+        alert(valorFinal);
+        break;
+    case("dam"):
+        valorFinal = medida*0.1;
+        alert(valorFinal);
+        break;
+    case("hm"):
+        valorFinal = medida*0.01;
+        alert(valorFinal);
+        break;
+    case("km"):
+        valorFinal = medida*0.001;
+        alert(valorFinal);
+        break;
+    default:
+        alert("Digite algo válido");
+        break;
+}
