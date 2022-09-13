87112519ae066256a6a7eff87c095f670b0fbe3b
Camandaroba06 <cristianogregorymc@gmail.com>
2022-09-13 13:48:36 -0300

    Css e comentar o JS todo

--- Exercicio4/index.js
+++ Exercicio4/index.js
@@ -1,42 +1,42 @@
-let arrayCidadesVisitadas = [];
-let arrayCidadesNAOVisitadas = [];
-let i=0;
-let i2 = 0;
-let visitou = true;
-let city, aux;
-let jorge = true;
-while(jorge === true){
-    city = window.prompt("Digite a Cidade");
-    aux = window.prompt("Visitou ? (responda com Sim ou Nao)");
-    if(aux === "Sim"){
-        visitou = true;
-    }else if(aux === "Nao"){
-        visitou = false;
-    }else{
-        alert("Digite algo Válido!!");
-    }
+// let arrayCidadesVisitadas = [];
+// let arrayCidadesNAOVisitadas = [];
+// let i=0;
+// let i2 = 0;
+// let visitou = true;
+// let city, aux;
+// let jorge = true;
+// while(jorge === true){
+//     city = window.prompt("Digite a Cidade");
+//     aux = window.prompt("Visitou ? (responda com Sim ou Nao)");
+//     if(aux === "Sim"){
+//         visitou = true;
+//     }else if(aux === "Nao"){
+//         visitou = false;
+//     }else{
+//         alert("Digite algo Válido!!");
+//     }
 
-    if(visitou === true){
-        arrayCidadesVisitadas[i] = city;
-        i++;
-    }else{
-        arrayCidadesNAOVisitadas[i2] = city;
-        i2++;
-    }
-    aux = window.prompt("Deseja Continuar o Programa ? (responda com Sim ou Nao)");
-    if(aux === "Sim"){
-        jorge = true;
-    }else if(aux === "Nao"){
-        jorge = false;
-    }else{
-        alert("Digite algo Válido!!");
-    }    
-}
-console.log("Cidades Visitadas:");
-for(i=0;i<arrayCidadesVisitadas.length;i++){
-    console.log(arrayCidadesVisitadas[i]);
-}
-console.log("Cidades NAO Visitadas:");
-for(i=0;i<arrayCidadesNAOVisitadas.length;i++){
-    console.log(arrayCidadesNAOVisitadas[i]);
-}
+//     if(visitou === true){
+//         arrayCidadesVisitadas[i] = city;
+//         i++;
+//     }else{
+//         arrayCidadesNAOVisitadas[i2] = city;
+//         i2++;
+//     }
+//     aux = window.prompt("Deseja Continuar o Programa ? (responda com Sim ou Nao)");
+//     if(aux === "Sim"){
+//         jorge = true;
+//     }else if(aux === "Nao"){
+//         jorge = false;
+//     }else{
+//         alert("Digite algo Válido!!");
+//     }    
+// }
+// console.log("Cidades Visitadas:");
+// for(i=0;i<arrayCidadesVisitadas.length;i++){
+//     console.log(arrayCidadesVisitadas[i]);
+// }
+// console.log("Cidades NAO Visitadas:");
+// for(i=0;i<arrayCidadesNAOVisitadas.length;i++){
+//     console.log(arrayCidadesNAOVisitadas[i]);
+// }
