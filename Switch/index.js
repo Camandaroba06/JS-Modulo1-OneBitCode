7dde4228d0d4f748243de8609d1d902648e6378b
Camandaroba06 <cristianogregorymc@gmail.com>
2022-09-13 12:11:38 -0300

    Switch

---
+++ Switch/index.js
@@ -0,0 +1,13 @@
+let x = window.prompt("Digite uma letra [a, c]");
+
+switch(x){
+    case("a"):
+        alert("O resultado é A");
+        break;
+    case("b"):
+        alert("O resultado é B");
+        break;
+    case("c"):
+        alert("O resultado é C");
+        break;
+}
