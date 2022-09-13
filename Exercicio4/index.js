45273004b86d7fed016d66e255fea42abecada55
Camandaroba06 <cristianogregorymc@gmail.com>
2022-09-13 13:32:42 -0300

    exercicio4

---
+++ Exercicio4/index.js
@@ -0,0 +1,24 @@
+let arrayCidadesVisitadas = [];
+let arrayCidadesNAOVisitadas = [];
+let i,i2 = 0;
+let visitou = true;
+let city, aux;
+while(visitou === true){
+    city = window.prompt("Digite a Cidade");
+    aux = window.prompt("Visitou ? (responda com Sim ou Nao)");
+    if(aux === "Sim"){
+        visitou = true;
+    }else if(aux === "Nao"){
+        visitou = false;
+    }else{
+        alert("Digite algo Válido!!");
+    }
+
+    if(visitou === true){
+        arrayCidadesVisitadas[i] = city;
+        i++;
+    }else{
+        arrayCidadesNAOVisitadas[i2] = city;
+        i2++;
+    }
+}
