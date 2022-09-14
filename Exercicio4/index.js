aea5085f1f8f28cbcf4c570ad830250c2741ba68
Cristiano Gregory Monfrim Camandaroba <cristianogregorymc@gmail.com>
2022-09-13 21:10:46 -0300

    Pegando o Array de visitadas e colocando na tabela do HTML

--- Exercicio4/index.js
+++ Exercicio4/index.js
@@ -1,42 +1,52 @@
-// let arrayCidadesVisitadas = [];
-// let arrayCidadesNAOVisitadas = [];
-// let i=0;
-// let i2 = 0;
-// let visitou = true;
-// let city, aux;
-// let jorge = true;
-// while(jorge === true){
-//     city = window.prompt("Digite a Cidade");
-//     aux = window.prompt("Visitou ? (responda com Sim ou Nao)");
-//     if(aux === "Sim"){
-//         visitou = true;
-//     }else if(aux === "Nao"){
-//         visitou = false;
-//     }else{
-//         alert("Digite algo Válido!!");
-//     }
+let arrayCidadesVisitadas = [];
+let arrayCidadesNAOVisitadas = [];
+let i = 0;
+let i2 = 0;
+let visitou = true;
+let city, aux;
+let jorge = true;
+while (jorge === true) {
+    city = window.prompt("Digite a Cidade");
+    aux = window.prompt("Visitou ? (responda com Sim ou Nao)");
+    if (aux === "Sim") {
+        visitou = true;
+    } else if (aux === "Nao") {
+        visitou = false;
+    } else {
+        alert("Digite algo Válido!!");
+    }
 
-//     if(visitou === true){
-//         arrayCidadesVisitadas[i] = city;
-//         i++;
-//     }else{
-//         arrayCidadesNAOVisitadas[i2] = city;
-//         i2++;
-//     }
-//     aux = window.prompt("Deseja Continuar o Programa ? (responda com Sim ou Nao)");
-//     if(aux === "Sim"){
-//         jorge = true;
-//     }else if(aux === "Nao"){
-//         jorge = false;
-//     }else{
-//         alert("Digite algo Válido!!");
-//     }    
-// }
-// console.log("Cidades Visitadas:");
-// for(i=0;i<arrayCidadesVisitadas.length;i++){
-//     console.log(arrayCidadesVisitadas[i]);
-// }
-// console.log("Cidades NAO Visitadas:");
-// for(i=0;i<arrayCidadesNAOVisitadas.length;i++){
-//     console.log(arrayCidadesNAOVisitadas[i]);
-// }
+    if (visitou === true) {
+        arrayCidadesVisitadas[i] = city;
+        i++;
+    } else {
+        arrayCidadesNAOVisitadas[i2] = city;
+        i2++;
+    }
+    aux = window.prompt("Deseja Continuar o Programa ? (responda com Sim ou Nao)");
+    if (aux === "Sim") {
+        jorge = true;
+    } else if (aux === "Nao") {
+        jorge = false;
+    } else {
+        alert("Digite algo Válido!!");
+    }
+}
+console.log("Cidades Visitadas:");
+let elementoVisita = document.getElementById("tbody");
+let elementoNaoVisitado = document.getElementById("naoVisitadas");
+for (i = 0; i < arrayCidadesVisitadas.length; i++) {
+    console.log(arrayCidadesVisitadas[i]);
+    var corpoTabela = document.querySelector('tbody');
+    var tr = document.createElement('tr');
+    var tdNome = document.createElement('td');
+    tdNome.textContent = arrayCidadesVisitadas[i];
+    tr.appendChild(tdNome);
+    corpoTabela.appendChild(tr);
+}
+console.log("Cidades NAO Visitadas:");
+for (i = 0; i < arrayCidadesNAOVisitadas.length; i++) {
+    console.log(arrayCidadesNAOVisitadas[i]);
+    // elementoNaoVisitado.innerHTML = arrayCidadesVisitadas[i];
+    document.write("<p id='visitadas'>" + arrayCidadesVisitadas[i] + "</p>");
+}
---
+++ teste/index.js
@@ -0,0 +1,24 @@
+var nome = 'Fulano';
+var email = 'coisodostreco@gmailcom';
+var cpf = '555.555.555-55';
+var data = '01/01/2020';
+
+
+var corpoTabela = document.querySelector('tbody');
+
+var tr = document.createElement('tr');
+var tdNome = document.createElement('td');
+var tdEmail = document.createElement('td');
+var tdCPF = document.createElement('td');
+var tdData = document.createElement('td');
+
+tdNome.textContent = nome;
+tdEmail.textContent = email;
+tdCPF.textContent = cpf;
+tdData.textContent = data;
+
+tr.appendChild(tdNome);
+tr.appendChild(tdEmail);
+tr.appendChild(tdCPF);
+tr.appendChild(tdData);
+corpoTabela.appendChild(tr);
